2a3f2326d5580660e62bf17ccb352141842a561a
07subhadip <subhadiphensh31102000@gmail.com>
2024-07-04 09:36:18 +0530

    commit changes

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -11,12 +11,11 @@ function App() {
     >
       <div className="fixed flex flex-wrap justify-center bottom-48 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-          <button onClick={()=>{setColor("red")}}className="outline-none px-4 rounded-full text-white font-bold shadow-lg"
+          <button onClick={()=>{setColor("red")}}className="outline-none px-4 rounded-full text-white font-bold shadow-lg "
           style={{backgroundColor: "red"}}>Red</button>
           <button onClick={()=>{setColor("yellow")}}className="outline-none px-4 rounded-full text-black font-bold shadow-lg"
           style={{backgroundColor: "yellow"}}>Yellow</button>
-          <button onClick={()=>{setColor("blue")}} className="outline-none px-4 rounded-full text-white font-bold shadow-lg"
-          style={{backgroundColor: "blue"}}>Blue</button>
+          <button onClick={()=>{setColor("blue")}} className="outline-none px-4 rounded-full text-white font-bold shadow-lg bg-sky-500 hover:bg-sky-700">Blue</button>
           <button onClick={()=>{setColor("green")}} className="outline-none px-4 rounded-full text-white font-bold shadow-lg"
           style={{backgroundColor: "green"}}>Green</button>
           <button onClick={()=>{setColor("magenta")}} className="outline-none px-4 rounded-full text-white font-bold shadow-lg"
